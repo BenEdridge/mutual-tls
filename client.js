940a27c7ac5522eb2ac0635e0fdee9b819c70a48
Ben Edridge <BenEdridge@users.noreply.github.com>
2019-07-09 23:48:40 +1000

    Basic Server and Client

--- client.js
+++ client.js
@@ -1,1 +1,25 @@
-// We can also setup a standard nodeJS client here as well
+const fs = require('fs');
+const https = require('https');
+
+const options = {
+    hostname: 'localhost',
+    port: 8443,
+    path: '/',
+    method: 'GET',
+    key: fs.readFileSync('keys/client.pem'),
+    cert: fs.readFileSync('keys/signed-client-crt.pem'),
+    ca: fs.readFileSync('keys/ca-crt.pem'),
+    //rejectUnauthorized: false,
+};
+
+const req = https.request(options, (res) => {
+    res.on('data', (data) => {
+        process.stdout.write(data);
+    });
+});
+
+req.end();
+
+req.on('error', (e) => {
+    console.error(e);
+});
--- server.js
+++ server.js
@@ -1,27 +1,34 @@
-const express = require('express');
 const fs = require('fs');
 const https = require('https');
+const express = require('express');
+
 const app = express();
+
 const options = {
     key: fs.readFileSync('keys/server-key.pem'),
     cert: fs.readFileSync('keys/server-crt.pem'),
     ca: fs.readFileSync('keys/signed-client-crt.pem'),
     requestCert: true,
-    rejectUnauthorized: false,
+    rejectUnauthorized: false, //if enabled you cannot do authentication based on client cert
     enableTrace: true,
     passphrase: '123456' //only if private key requires?
 };
 
-app.use((req, res, next) => {
+app.use((req, res) => {
     if(!req.client.authorized){
-        return res.status(401).send('NO ACCESS');
+        return res.status(401).send('ACCESS DENIED');
     }
-    res.writeHead(200);
-    res.end("hello world\n");
-    next();
+    //examine the cert itself, and even validate based on that!
+    const cert = req.socket.getPeerCertificate();
+    if (cert.subject) {
+       console.log(cert.subject.CN);
+    }
+    res.send('You have logged in');
+    //res.writeHead(200);
+    //res.end("hello world\n");
+    //next();
 });
 
-const listener = https.createServer(options, app).listen(4433, () => {
+const listener = https.createServer(options, app).listen(8443, () => {
     console.log('Express HTTPS server listening on port ' + listener.address().port);
 });
-
