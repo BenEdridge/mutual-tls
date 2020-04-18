8c2bf32ee0cf63a6601b2c3aa0fea533614b6d39
Ben Edridge <BenEdridge@users.noreply.github.com>
2020-04-19 00:17:36 +1000

    Test splitting

--- tests/server_http.test.js
+++ tests/server_http.test.js
@@ -8,11 +8,10 @@ const config = require('../src/config');
 
 const badOptions = {
   host: config.clientConfig.host,
-  port: config.clientConfig.host,
+  port: config.clientConfig.port,
   key: fs.readFileSync('./tests/bad_keys/CLIENT_key.pem'),
   cert: fs.readFileSync('./tests/bad_keys/CLIENT.crt'),
   ca: fs.readFileSync('./tests/bad_keys/CA.crt'),
-  minVersion: config.clientConfig.minVersion,
 };
 
 const server = require('../src/server_http').init();
@@ -53,35 +52,35 @@ tap.test('http/2 gets a successful response with valid certs', (t) => {
   });
 });
 
-// tap.test('http/1 returns an error for invalid certs', (t) => {
-//   https.get(badOptions, (res) => {
-//     res.on('data', (d) => {
-//       t.fail('Connection Should Fail');
-//     });
-//   }).on('error', (e) => {
-//     t.equal(e.toString(), 'Error: certificate signature failure');
-//   }).on('close', () => {
-//     t.end();
-//   })
-// });
+tap.test('http/1 returns an error for invalid certs', (t) => {
+  https.get(badOptions, (res) => {
+    res.on('data', (d) => {
+      t.fail('Connection Should Fail');
+    });
+  }).on('error', (e) => {
+    t.equal(e.toString(), 'Error: certificate signature failure');
+  }).on('close', () => {
+    t.end();
+  })
+});
 
-// tap.test('http/2 returns an error for invalid certs', (t) => {
+tap.test('http/2 returns an error for invalid certs', (t) => {
 
-//   const clientHttp2Session = http2.connect('https://127.0.0.1:8443', badOptions);
-//   const clientHttp2Stream = clientHttp2Session.request();
+  const clientHttp2Session = http2.connect('https://127.0.0.1:8443', badOptions);
+  const clientHttp2Stream = clientHttp2Session.request();
 
-//   t.plan(2);
+  t.plan(2);
 
-//   clientHttp2Stream.on('error', (error) => {
-//     t.equal(error.message, 'The pending stream has been canceled (caused by: certificate signature failure)');
-//   });
+  clientHttp2Stream.on('error', (error) => {
+    t.equal(error.message, 'The pending stream has been canceled (caused by: certificate signature failure)');
+  });
 
-//   clientHttp2Session.on('error', (error) => {
-//     t.equal(error.message, 'certificate signature failure');
-//     t.end();
-//   });
-// });
+  clientHttp2Session.on('error', (error) => {
+    t.equal(error.message, 'certificate signature failure');
+    t.end();
+  });
+});
 
 tap.tearDown(() => {
-  server.http2Server.close();
+  server.close();
 })
--- tests/server_wss.test.js
+++ tests/server_wss.test.js
@@ -1,33 +1,30 @@
 'use strict';
 
 const tap = require('tap');
-const https = require('https');
-const http2 = require('http2');
 const websocket = require('ws');
 const fs = require('fs');
 const config = require('../src/config');
 
 const badOptions = {
   host: config.clientConfig.host,
-  port: config.clientConfig.host,
+  port: config.clientConfig.port,
   key: fs.readFileSync('./tests/bad_keys/CLIENT_key.pem'),
   cert: fs.readFileSync('./tests/bad_keys/CLIENT.crt'),
   ca: fs.readFileSync('./tests/bad_keys/CA.crt'),
-  minVersion: config.clientConfig.minVersion,
 };
 
 const wss = require('../src/server_wss').init();
 
-tap.test('WebSocket gets a successful response with valid certs', (t) => {
+// tap.test('WebSocket gets a successful response with valid certs', (t) => {
 
-  const ws = new websocket(`wss://${config.clientConfig.host}:${config.clientConfig.port}`, config.clientConfig);
+//   const ws = new websocket(`wss://${config.clientConfig.host}:${config.clientConfig.port}`, config.clientConfig);
 
-  ws.on('message', (data) => {
-    t.equal(data.toString(), 'Welcome to Mutual-TLS Websockets!');
-    ws.terminate(); // immediately close
-    t.end();
-  });
-});
+//   ws.on('message', (data) => {
+//     t.equal(data.toString(), 'Welcome to Mutual-TLS Websockets!');
+//     ws.terminate(); // immediately close
+//     t.end();
+//   });
+// });
 
 tap.test('WebSocket returns an error for invalid certs', (t) => {
   const ws = new websocket(`wss://127.0.0.1:8443`, badOptions);
@@ -36,15 +33,24 @@ tap.test('WebSocket returns an error for invalid certs', (t) => {
     t.fail('Should not receive data!');
   });
 
-  ws.on('error', (error) => {
-    t.equal(error.toString(), 'Error: certificate signature failure');
+  ws.on('upgrade', (message) => {
+    t.fail('Should not receive data!');
   });
 
-  ws.on('close', () => {
+  ws.on('error', (error) => {
+    t.equal(error.toString(), 'Error: certificate signature failure');
+    ws.terminate();
+    wss.close();
     t.end();
   });
+
+  ws.on('unexpected-response', (req, res) => {
+    t.fail('Should not receive data!');
+  });
+
 });
 
 tap.tearDown(() => {
+  wss.removeAllListeners();
   wss.close();
 });
