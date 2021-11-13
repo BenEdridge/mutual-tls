0d9fe57d4e23f7116415382e899876e69f6a6cec
Ben Edridge <BenEdridge@users.noreply.github.com>
2021-11-13 15:47:22 +1100

    Fix wscat script

--- src/scripts/wscat.sh
+++ src/scripts/wscat.sh
@@ -1,1 +1,4 @@
-wscat -c wss://localhost:8443 --ca keys/CA.crt  --cert keys/CLIENT.crt --key keys/CLIENT_key.pem
+#!/bin/sh
+
+# See: https://github.com/websockets/wscat
+npx wscat -c wss://localhost:8443 --ca keys/CA.crt  --cert keys/CLIENT.crt --key keys/CLIENT_key.pem
